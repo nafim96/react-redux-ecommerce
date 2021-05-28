26f3e2601f4489f5725f63c3c3f2020d30f7ef60
Rabius Sunny <rabiussunnyc3x@gmail.com>
2021-05-28 23:29:33 +0600

    Homepage, Details Page and Redux Integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
-import './app.css'
-const App = () => {
+import Header from './components/Header'
+import './app.css';
+import Intro from './components/Intro';
+import Products from './components/Products';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+import DetailProduct from './components/Products/DetailProduct';
+
+function App() {
   return (
-    <div>
-      <h1>We made some editing.</h1>
+    <div className="App">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact>
+            <Intro />
+            <Products />
+          </Route>
+          <Route path="/product/:id">
+            <DetailProduct />
+          </Route>
+        </Switch>
+      </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Intro/index.jsx
@@ -0,0 +1,12 @@
+import { Jumbotron } from "react-bootstrap"
+import classes from './intro.module.css'
+
+const Intro = () => {
+    return (
+        <div className="container">
+            <h1>Hi, Welcome to Redux Mart</h1>
+        </div>
+    )
+}
+
+export default Intro
