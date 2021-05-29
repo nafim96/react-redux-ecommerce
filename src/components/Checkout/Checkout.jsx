6a6a0a36a13ddcd94df5b9355ee5fbb9c2e3f0aa
Rabius Sunny <rabiussunnyc3x@gmail.com>
2021-05-30 04:15:40 +0600

    Checkout Structure

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -5,15 +5,73 @@ const Checkout = () => {
 
     const user = useSelector(state => state.user.user)
     let product = useSelector(state => state.singleProduct.product)
-    const {title, price} = product
+    const { title, price, image, description } = product
+    let vat = price * 10 / 100
     console.log(product)
 
     return (
         <>
             <section className="checkout">
-                <div className="container">
-                    <h1>Hi {user.name} !</h1>
-                    <p>You've selected - "{title}"</p>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="productPart">
+                                <div className="d-flex justify-content-center">
+                                    <h5>{title}</h5>
+                                    <p>${price}</p>
+                                </div>
+                                <div className="text-center">
+                                    <img className="w-50" src={image} alt="" />
+                                </div>
+                                <div>
+                                    <p className="pt-5">{description}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="paymentPart">
+                                <h4>Payment Details</h4>
+                                <p>Complete your purchase by providing your payment details</p>
+                                <form className="pt-5">
+                                    <div class="form-group mb-3">
+                                        <label htmlFor="name">Your Name</label>
+                                        <input type="text" name="name" class="form-control" id="name" defaultValue={user.name} required />
+                                    </div>
+                                    <div class="form-group mb-3">
+                                        <label htmlFor="email">Your Email</label>
+                                        <input type="text" name="email" class="form-control" id="first" defaultValue={user.email} required />
+                                    </div>
+                                    <label htmlFor="address">Billing Address</label>
+                                    <input type="text" name="address" class="form-control" id="address" placeholder="United States" required />
+                                    <div class="input-group">
+                                        <input type="number" name="zip" class="form-control" placeholder="ZIP" required />
+                                        <input type="text" name="state" class="form-control" placeholder="State" required />
+                                    </div>
+                                    <div className="price my-4">
+                                        <table>
+                                            <tbody>
+                                                <tr>
+                                                    <td className="py-2">Subtotal</td>
+                                                    <td>${price}</td>
+                                                </tr>
+                                                <tr>
+                                                    <td className="py-2">VAT (10%)</td>
+                                                    <td>${vat}</td>
+                                                </tr>
+                                                <tr>
+                                                    <td className="py-2"><b>Total</b></td>
+                                                    <td><b>{price + vat}</b></td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <div class="d-grid my-5">
+                                        <input class="btn btn-primary" type="submit" value={'Pay $' + (price + vat)} />
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </section>
         </>
